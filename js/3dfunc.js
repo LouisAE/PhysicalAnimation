63b6420c59fe7fa80be8424247642d668fc3980c
LouisZ <88891403+LouisAE@users.noreply.github.com>
2023-01-21 14:03:27 +0800

    add drawBox function

--- js/3dfunc.js
+++ js/3dfunc.js
@@ -1,17 +1,22 @@
 import * as THREE from "three";
 
 export function DrawSphere(radius,color){
-  let SphereGeometry = new THREE.SphereGeometry( radius, 64, 16 );
-  let SphereMaterial = new THREE.MeshBasicMaterial( { color: color } );
-  return new THREE.Mesh( SphereGeometry, SphereMaterial );
+  let geometry = new THREE.SphereGeometry( radius, 64, 16 );
+  let material = new THREE.MeshBasicMaterial( { color: color } );
+  return new THREE.Mesh( geometry, material );
 }
 
 export function DrawRing(innerRadius,outerRadius,color){
-  const RingGeometry = new THREE.RingGeometry( innerRadius, outerRadius, 64 );
-  const RingMaterial = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
-  return new THREE.Mesh( RingGeometry, RingMaterial );
+  const geometry = new THREE.RingGeometry( innerRadius, outerRadius, 64 );
+  const material = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
+  return new THREE.Mesh( geometry, material );
 }
 
+export function DrawBox(width, height, depth, color) {
+  const geometry = new THREE.BoxGeometry(width, height, depth);
+  const material = new THREE.MeshBasicMaterial({ color: color });
+  return new THREE.Mesh(geometry, material);
+}
 
 
 
