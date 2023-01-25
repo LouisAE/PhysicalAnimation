8a2485a51b642cc2d0d7279405409e81870b1d99
LouisZ <88891403+LouisAE@users.noreply.github.com>
2023-01-25 21:33:57 +0800

    complete simple pendulum

--- js/main.js
+++ js/main.js
@@ -24,19 +24,21 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const light = new THREE.AmbientLight(0x404040); // soft white light
 scene.add(light);
 
-let m = 1,theta = rad(60),len = 15,dt = 1/60;
 
-let smallBall = DrawSphere(0.5,0xff0000);
+let m = 100, theta = rad(30), len = 15, dt = 1 / 60, ani, running = true;
 
-let bigBall = DrawSphere(2,0x0ef76f);
+
+let smallBall = DrawSphere(0.5, 0xff0000);
+
+let bigBall = DrawSphere(2, 0x0ef76f);
 smallBall.position.y = len;
 bigBall.position.x = len * Math.sin(theta);
 bigBall.position.y = len - (len * Math.cos(theta));
 scene.add(smallBall);
 scene.add(bigBall);
 
-let floor = DrawBox(50,3,2,0xffffff);
-floor.position.y = -10;
+let floor = DrawBox(50, 3, 2, 0xffffff);
+floor.position.y = -30;
 scene.add(floor);
 
 //细线
@@ -46,7 +48,7 @@ string0.rotateZ(theta);
 scene.add(string0);
 
 
-
+//物理世界
 let world = new CANNON.World();
 world.gravity.set(0, -9.8, 0);
 world.broadphase = new CANNON.NaiveBroadphase();
@@ -55,18 +57,18 @@ let bodyBigBall = new CANNON.Body({
   mass: m,
   position: new CANNON.Vec3(len * Math.sin(theta), len - (len * Math.cos(theta)), 0),
   shape: new CANNON.Sphere(2),
-  material: new CANNON.Material({restitution:0})
+  material: new CANNON.Material({restitution: 0})
 });
 bigBall.userData = bodyBigBall;
-bodyBigBall.linearDamping=0;
-bodyBigBall.angularDamping=0;
+bodyBigBall.linearDamping = 0;
+bodyBigBall.angularDamping = 0;
 world.addBody(bodyBigBall);
 
 let bodyFloor = new CANNON.Body({
   mass: 0,
   position: new CANNON.Vec3(0, -10, 0),
   shape: new CANNON.Box(new CANNON.Vec3(25, 1.5, 1)),
-  material: new CANNON.Material({friction:0})
+  material: new CANNON.Material({friction: 0})
 });
 floor.userData = bodyFloor;
 world.addBody(bodyFloor);
@@ -81,34 +83,27 @@ let bodySmallBall = new CANNON.Body({
 smallBall.userData = bodySmallBall;
 world.addBody(bodySmallBall);
 
-/*
-let bodyString0 = new CANNON.Body({
-  mass: 0.1,
-  position: new CANNON.Vec3(string0.position.x, string0.position.y, string0.position.z),
-  shape: new CANNON.Cylinder(0.1, 0.1, len, 10),
-  material: new CANNON.Material()
-});
-string0.userData = bodyString0;
-world.addBody(bodyString0);
+world.addConstraint(new CANNON.PointToPointConstraint(bodySmallBall, new CANNON.Vec3(0, 0, 0), bodyBigBall, new CANNON.Vec3(-len * Math.sin(theta), len * Math.cos(theta), 0)));
 
-
-//world.addConstraint(new CANNON.PointToPointConstraint(bodyString0,new CANNON.Vec3(0,len/2,0), bodySmallBall,new CANNON.Vec3(0,0,0)));
-*/
-
-world.addConstraint(new CANNON.PointToPointConstraint(bodySmallBall,new CANNON.Vec3(0,0,0), bodyBigBall,new CANNON.Vec3(-len*Math.sin(theta),len*Math.cos(theta),0)));
 
 function animate() {
-  //ani = requestAnimationFrame(animate);
+  ani = requestAnimationFrame(animate);
   renderer.render(scene, camera);
   world.step(dt);
-  for(let i in scene.children) {
+
+  for (let i in scene.children) {
     if (scene.children[i].isMesh && scene.children[i].userData.position) {
       scene.children[i].position.copy(scene.children[i].userData.position);
       scene.children[i].quaternion.copy(scene.children[i].userData.quaternion);
     }
   }
+
+  string0.position.copy(bigBall.position.clone().add(smallBall.position).multiplyScalar(0.5));
+  string0.rotation.z = theta;
+
+  theta = Math.atan2(bigBall.position.x, len - bigBall.position.y);
   controls.update();
 }
-//animate();
-renderer.setAnimationLoop(animate);
+
+animate();
 
