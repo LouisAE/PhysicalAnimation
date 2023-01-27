9437e410ba05335952859e3f8f18165039a873c4
LouisZ <88891403+LouisAE@users.noreply.github.com>
2023-01-27 17:36:15 +0800

    局部优化

--- js/functions.js
+++ js/functions.js
@@ -7,20 +7,20 @@ export function drawSphere(radius,color){
 }
 
 export function drawRing(innerRadius,outerRadius,color){
-  const geometry = new THREE.RingGeometry( innerRadius, outerRadius, 64 );
-  const material = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
+  let geometry = new THREE.RingGeometry( innerRadius, outerRadius, 64 );
+  let material = new THREE.MeshBasicMaterial( { color: color, side: THREE.DoubleSide } );
   return new THREE.Mesh( geometry, material );
 }
 
 export function drawBox(width, height, depth, color) {
-  const geometry = new THREE.BoxGeometry(width, height, depth);
-  const material = new THREE.MeshBasicMaterial({ color: color });
+  let geometry = new THREE.BoxGeometry(width, height, depth);
+  let material = new THREE.MeshBasicMaterial({ color: color });
   return new THREE.Mesh(geometry, material);
 }
 
 export function drawCylinder(radius, height, color) {
-  const geometry = new THREE.CylinderGeometry(radius, radius, height);
-  const material = new THREE.MeshBasicMaterial({ color: color });
+  let geometry = new THREE.CylinderGeometry(radius, radius, height);
+  let material = new THREE.MeshBasicMaterial({ color: color });
   return new THREE.Mesh(geometry, material);
 }
 
